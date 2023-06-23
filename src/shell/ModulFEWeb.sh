954b0d0bbbbe29536f9a073bba974e7fe038c06a
Dimas Seto Wicaksana <31839535+Shironim@users.noreply.github.com>
2023-06-24 00:29:05 +0700

    Update ModulFEWeb.sh

--- src/shell/ModulFEWeb.sh
+++ src/shell/ModulFEWeb.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-pkexec sudo apt-get install nodejs -y && pkexec snap install code --classic && pkexec snap install chromium
+pkexec sudo apt-get install nodejs -y  && pkexec snap install chromium
