ee9057a383a89e09b90cf4ec6fcfe62d20fd8396
Dimas Seto Wicaksana <31839535+Shironim@users.noreply.github.com>
2023-06-24 00:02:26 +0700

    Update ModulBEWeb.sh

--- src/shell/ModulBEWeb.sh
+++ src/shell/ModulBEWeb.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-sudo apt-get install python3 php mariadb-server apache2 postgresql postgresql-contrib composer -y && sudo snap install code --classic
+pkexec apt-get install python3 php mariadb-server apache2 postgresql postgresql-contrib composer -y && pkexec snap install code --classic
